2fded42ee56a0b3d95c0f540e5a6516b61970b4b
DiegoMerinoMas <u202111473@upc.edu.pe>
2023-10-29 16:49:34 -0500

    Fixed and better practices

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:kindcoins_flutterapp/screens/create-campaign/create_campaign_1.dart';
 import 'package:kindcoins_flutterapp/screens/welcome/welcome_screen.dart';
 
 void main() => runApp(MyApp());
@@ -7,10 +8,10 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context){
-    return MaterialApp(
+  Widget build(BuildContext context) {
+    return const MaterialApp(
       title: "KindCoins",
-      home: WelcomeScreen(),
+      home: CampaignCreationPage(),
       debugShowCheckedModeBanner: false,
     );
   }
