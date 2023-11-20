cc69f1562cc320657b49dc8d5131e70b2dbdfc83
Markollantes2307 <u201410183@upc.edu.pe>
2023-11-20 17:12:40 -0500

    fix: update for profile screen

--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -39,76 +39,142 @@ class _ProfileScreen extends State<ProfileScreen> {
         backgroundColor: Color.fromARGB(255, 5, 151, 166),
         title: Text('Perfil'),
       ),
-      body: ListView(
-        children: [
-          // Sección de información del usuario
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
+      body: FutureBuilder<User>(
+        future: user,
+        builder: (context,snapshot){
+          if(snapshot.hasData){
+            User? current=snapshot.data;
+            return ListView(
               children: [
-                CircleAvatar(
-                  backgroundImage: AssetImage('assets/images/profile/cat-profile.jpg'),
-                  radius: 50,
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      CircleAvatar(
+                        backgroundImage: AssetImage(current!.photo),
+                        radius: 50,
+                      ),
+                      SizedBox(height: 10),
+                      Text(current.firstName + ' ' + current.lastName),
+                      Text('Lima, Perú'),
+                    ],
+                  ),
                 ),
-                SizedBox(height: 10),
-                Text('Usuario prueba'),
-                Text('Lima, Perú'),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Medallas'),
+                      Row(
+                        children: [
+                          Image.asset('assets/images/profile/medalla-oro.png', height: 30, width: 30),
+                          Text('Oro'),
+                          // Agrega más medallas según sea necesario
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Donaciones Realizadas'),
+                      CampaignCard(
+                        title: 'Alimenta familias en palestina',
+                        description: 'Las comidas compartidas proporcionarán a las familias de Palestina un apoyo en efectivo para que puedan comprar alimentos en tiendas locales.',
+                        imagePath: 'assets/images/profile/palestina-niña.png',
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Campañas Propias'),
+                      CampaignCard(
+                        title: 'Ayuda a las ollas comunes',
+                        description: 'Dona dinero o alimentos a las ollas comunes de San Juan de Lurigancho.',
+                        imagePath: 'assets/images/profile/ollas-comunes-peru.jpg',
+                      ),
+                      // Agregar más campañas propias
+                    ],
+                  ),
+                ),
               ],
-            ),
-          ),
-
-          // Sección de medallas
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+            );
+          }else{
+            return ListView(
               children: [
-                Text('Medallas'),
-                Row(
-                  children: [
-                    Image.asset('assets/images/profile/medalla-oro.png', height: 30, width: 30),
-                    Text('Oro'),
-                    // Agrega más medallas según sea necesario
-                  ],
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      CircleAvatar(
+                        backgroundImage: AssetImage('assets/images/profile/cat-profile.jpg'),
+                        radius: 50,
+                      ),
+                      SizedBox(height: 10),
+                      Text('Usuario prueba'),
+                      Text('Lima, Perú'),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Medallas'),
+                      Row(
+                        children: [
+                          Image.asset('assets/images/profile/medalla-oro.png', height: 30, width: 30),
+                          Text('Oro'),
+                          // Agrega más medallas según sea necesario
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Donaciones Realizadas'),
+                      CampaignCard(
+                        title: 'Alimenta familias en palestina',
+                        description: 'Las comidas compartidas proporcionarán a las familias de Palestina un apoyo en efectivo para que puedan comprar alimentos en tiendas locales.',
+                        imagePath: 'assets/images/profile/palestina-niña.png',
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Campañas Propias'),
+                      CampaignCard(
+                        title: 'Ayuda a las ollas comunes',
+                        description: 'Dona dinero o alimentos a las ollas comunes de San Juan de Lurigancho.',
+                        imagePath: 'assets/images/profile/ollas-comunes-peru.jpg',
+                      ),
+                      // Agregar más campañas propias
+                    ],
+                  ),
                 ),
               ],
-            ),
-          ),
-
-          // Sección de donaciones
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text('Donaciones Realizadas'),
-                CampaignCard(
-                  title: 'Alimenta familias en palestina',
-                  description: 'Las comidas compartidas proporcionarán a las familias de Palestina un apoyo en efectivo para que puedan comprar alimentos en tiendas locales.',
-                  imagePath: 'assets/images/profile/palestina-niña.png',
-                ),
-              ],
-            ),
-          ),
-
-          // Sección de campañas propias
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text('Campañas Propias'),
-                CampaignCard(
-                  title: 'Ayuda a las ollas comunes',
-                  description: 'Dona dinero o alimentos a las ollas comunes de San Juan de Lurigancho.',
-                  imagePath: 'assets/images/profile/ollas-comunes-peru.jpg',
-                ),
-                // Agregar más campañas propias
-              ],
-            ),
-          ),
-        ],
+            );;
+          }
+        }
       ),
     );
   }
