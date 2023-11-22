ff43ece4cf041bc4751ba6c1f2bcf25514623702
DiegoMerinoMas <u202111473@upc.edu.pe>
2023-11-22 03:55:54 -0500

    cambion en el main para que corra

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:kindcoins_flutterapp/screens/create-campaign/create_campaign_1.dart';
 import 'package:kindcoins_flutterapp/screens/welcome/welcome_screen.dart';
+import 'package:kindcoins_flutterapp/screens/create-campaign/components/campaign_provider.dart';
+import 'package:provider/provider.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => CampaignProvider(),
+        ),
+        // Otros providers si los tienes...
+      ],
+      child: MyApp(),
+    ),
+  );
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: "KindCoins",
       home: WelcomeScreen(),
       debugShowCheckedModeBanner: false,
